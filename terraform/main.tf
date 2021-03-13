2785424b681ba8195a278ff3f0294b7a9ae9c892
Mikael Kohlmyr <mkohlmyr@protonmail.com>
2021-03-13 23:00:46 +0000

    asd

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,45 +2,18 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-resource "aws_iam_user" "user" {
-  name = "github"
-}
+resource "aws_s3_bucket" "state" {
+  bucket = "actionable-terraform-remote-state"
 
-resource "aws_iam_policy" "policy" {
-  name        = "github-policy"
-  policy      = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Effect": "Allow",
-                "Action": [
-                  "iam:DeletePolicy",
-                  "iam:UpdatePolicy",
-                  "iam:CreatePolicy",
-                  "iam:GetUserPolicy",
-                  "iam:PutUserPolicy",
-                  "iam:DeleteUserPolicy",
-                  "iam:AttachUserPolicy",
-                  "iam:DetachUserPolicy",
-                  "iam:GetUser",
-                  "iam:CreateUser",
-                  "iam:UpdateUser"
-                ],
-                "Resource": [
-                    "arn:aws:iam::278755489513:user/*",
-                    "arn:aws:iam::278755489513:policy/*"
-                ]
-            }
-        ]
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
       }
-    ]
-  })
-}
-
-resource "aws_iam_user_policy_attachment" "test-attach" {
-  user       = aws_iam_user.user.name
-  policy_arn = aws_iam_policy.policy.arn
+    }
+  }
 }
