ad2656fc50f068e21625d3957e925d856dea0fdd
Mikael Kohlmyr <mkohlmyr@protonmail.com>
2021-03-14 01:02:57 +0000

    state

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,22 +2,10 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-resource "aws_iam_user" "user" {
-  name = "ci"
-}
-
-resource "aws_s3_bucket" "state" {
-  bucket = "actionable-terraform-remote-state"
-
-  versioning {
-    enabled = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
+terraform {
+  backend "s3" {
+    bucket = "actionable-terraform-remote-state"
+    key    = "actionable/terraform.tfstate"
+    region = "eu-west-2"
   }
 }
